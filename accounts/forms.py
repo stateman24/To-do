865ea7a054d718ac149399be960508a6ad3749fb
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-08-12 19:33:21 +0100

    Finished styling the login and sign-up page

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,13 +6,58 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", "first_name", "email", "password1",)
+        fields = ("username", "first_name", "email", "password1", "password2")
 
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "id": "username",
+            "class": "username",
+            "placeholder": "Your username goes here"
+        }
+    ))
+
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "id": "first-name",
+            "class": "firstname",
+            "placeholder": "Your name goes here"
+        }
+    ))
+
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "id": "email-address",
+            "class": "email",
+            "placeholder": "Your email goes here",
+            "required": True
+        },
+    ))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "id": "password",
+            "class": "pass",
+            "placeholder": "Your password goes here"
+        }
+    ))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "id": "password2",
+            "class": "pass2",
+            "placeholder": "Confirm your password"
+        }
+    ))
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password2"] != cd["password1"]:
+            return forms.ValidationError("Password Mismatch")
+        return cd["password2"]
 
     def clean_email(self):
         data = self.cleaned_data['email']
-        if User.objects.filter(email=data).exist:
+        if User.objects.filter(email=data).exists():
             return forms.ValidationError("Email already used")
         return data
 
