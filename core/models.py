16f8a7a690d3d8f0b48ef3274d23b7812949ff2d
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-08-13 19:35:02 +0100

    Styled the base.html file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    name = models.CharField(max_length=225)
+    status = models.BooleanField(default=False,)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
